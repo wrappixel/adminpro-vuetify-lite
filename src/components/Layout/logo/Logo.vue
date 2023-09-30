29e694b72c84a0968a5e3cd199f692fb2abc5e89
adminmart <info@adminmart.com>
2023-09-30 12:20:03 +0530

    'update'

---
+++ src/components/Layout/logo/Logo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+<template>
+    <div class="logo">
+      <NuxtLink to="/" class="d-flex">
+        <img src="@/assets/images/logos/dark-logo-icon.svg" />
+        <img src="@/assets/images/logos/dark-logo-text.svg" class="pl-3" />
+      </NuxtLink>
+    </div>
+  </template>
